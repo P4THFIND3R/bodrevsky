3c2fcdf38124b8cc3c57af8244061148ff24c965
Vladislav Bodrevskiy <anpathfinder@mail.ru>
2024-12-27 17:05:12 +0300

    feat: multiply workers

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.include_router(youtube_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run("src.main:app", host="0.0.0.0", port=8080, workers=2)
